7ad661e01288e28138b1a7a40975d56f0fcafe95
Vlad <ver_peta@mail.ru>
2014-03-10 21:56:23 +0200

    Working script
    Single sending  SMS

--- demo.php
+++ demo.php
@@ -1,45 +1,31 @@
 <?php
-$from="demo"; //Ваше альфаимя!
-$to="380500839929";//Номер получателя
-$text="Тестовое сообщение";
-$login='demo';//Логин 
-$password='demo';//Пароль
-$url='http://sms.businesslife.com.ua/clients.php';//URL для отправки сообщений
-$xml="<message><service id='single' source='$from'/><to>$to</to><body content-type='text/plain' encoding='plain'>$text</body></message>";
-
-$answ=post_request($xml, $url, $login, $password);
-
-echo "$answ\n";
-var_dump($answ);
-
-function post_request($data, $url, $login, $pwd)
+function post_request($url, $login, $pwd)
 {
-        $credent = sprintf('Authorization: Basic %s',base64_encode($login.":".$pwd) );
-        $params=array('http'=>array('method'=>'POST','content'=>$data, 'header'=>$credent));
-        $ctx = stream_context_create($params);
-        $fp=@fopen($url, 'rb', FALSE, $ctx);
-        if ($fp)
-        {
-                $response = @stream_get_contents($fp);
-                return $response;
-        }
-        else return FALSE;
+$params = array('http' =>
+array(
+'method' => 'POST',
+'header' => array('Authorization: Basic ' . base64_encode($login.":".$pwd),'Content-Type: text/xml'),
+'content' => '<message><service id="single" source="DEMO"/><to>+380500839929</to>
+<body content-type="text/plain">Test from demo</body></message>'
+));
+ 
+#var_dump ($params);
+ 
+ 
+$ctx = stream_context_create($params);
+$fp=@fopen($url, 'rb', FALSE, $ctx);
+if ($fp) {
+$response = @stream_get_contents($fp);
+return $response;
+}else{
+return FALSE;
 }
-#Ошибки при создании рассылок по протоколу IP2SMS.
-/*
-Ошибки передаваемые не в XML:
-401 Unauthorized – ошибка авторизации
-Request not recognized - xml parse failed. Request text: … -- некорректный XML.
-
-Ошибки передаваемые в XML:
-<status date=''><state error='uniq_key dupe'>Rejected</state></status> -- не уникальный ключ рассылки
-state error='Unknown operator code {kod}'>Rejected</state> -- не верный/неподдерживаемый оператор
-<state error='Message text not found'>Rejected</state>
-<state error='Invalid abonent number'>Rejected</state>
-<state error='Prepaid messages limit exceed'>Rejected</state> -- Если клиент подключен по prepaid, нет средств на счету
-<state error='Prepaid bill error'>Rejected</state> -- Если клиент подключен по prepaid,  ошибка биллинга (например, не назначен тариф)
-<state error='Text don`t match allowed templates'>Rejected</state></status>
-<state error='2 or more numbers required for bulk or individual mode'>Rejected</state> -- В режиме bulk и individual нельзя отправлять сообщение одному абоненту.
-<state error='Invalid source number'>Rejected</state> -- альфимя не корректно или запрещено/не активировано.
-*/
+}
+ 
+$login = 'demo';
+$password = 'demo';
+$answ = post_request('https://api.life.com.ua/ip2sms/', $login, $password);
+ 
+echo $answ;
+exit;
 ?>
